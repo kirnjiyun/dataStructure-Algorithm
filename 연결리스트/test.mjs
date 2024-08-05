7e8d49593cc66d5aa7c482d3cd9211a5174162ea
kirnjiyun <kimjiyunee@naver.com>
2024-08-05 16:13:58 +0900

    연결리스트

---
+++ 연결리스트/test.mjs
@@ -0,0 +1,41 @@
+import { Node, LinkedList } from "./연결리스트.mjs";
+
+let node1 = new Node(1);
+let node2 = new Node(2);
+let node3 = new Node(3);
+node1.next = node2;
+node2.next = node3;
+
+console.log(node1.data);
+console.log(node1.next.data);
+console.log(node1.next.next.data);
+
+let list = new LinkedList();
+console.log("insertAta호출");
+list.insertAt(0, 0);
+list.insertAt(1, 1);
+list.insertAt(2, 2);
+list.insertAt(3, 3);
+list.insertAt(4, 4);
+list.printAll();
+console.log("clear, printAll 호출");
+list.clear();
+list.printAll();
+console.log("insertLast호출");
+list.insertLast(0);
+list.insertLast(100);
+list.insertLast(1000);
+list.insertLast(1234);
+list.printAll();
+console.log("deleteAt호출");
+list.deleteAt(0);
+list.printAll();
+list.deleteAt(1);
+list.printAll();
+console.log("deleteLast호출");
+list.printAll();
+list.deleteLast();
+list.printAll();
+console.log("getNodeAt호출");
+let zeroNode = list.getNodeAt(0);
+console.log(zeroNode);
